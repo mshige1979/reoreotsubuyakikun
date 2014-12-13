4cb95bf485e8cbf7684430828a0e2bef64237f04
mshige1979 <m_shige1979@r2.dion.ne.jp>
2014-12-13 13:39:05 +0900

    first commit

---
+++ app.psgi
@@ -0,0 +1,51 @@
+use Mojolicious::Lite;
+use DateTime;
+use Encode;
+use JSON;
+use utf8;
+
+# 接続人数
+my $clients = {};
+
+# Template with browser-side code
+get '/' => 'index';
+
+# WebSocket echo service
+websocket '/echo' => sub {
+  my $c = shift;
+
+  my $id = sprintf "%s", $c->tx;
+  $clients->{$id} = $c->tx;
+
+  # Opened
+  $c->app->log->debug('WebSocket opened.');
+
+  # Increase inactivity timeout for connection a bit
+  $c->inactivity_timeout(300);
+
+  # Incoming message
+  $c->on(message => sub {
+    my ($c, $msg) = @_;
+
+    $c->app->log->debug('mesage->' . $msg);
+
+    my $dt   = DateTime->now( time_zone => 'Asia/Tokyo');
+
+    for (keys %$clients) {
+      my $_msg = "$msg";
+      $clients->{$_}->send(JSON->new->utf8(0)->encode({
+        hms  => $dt->hms,
+        text => $_msg,
+       }));
+    }
+
+  });
+
+  # Closed
+  $c->on(finish => sub {
+    my ($c, $code, $reason) = @_;
+    $c->app->log->debug("WebSocket closed with status $code.");
+  });
+};
+
+app->start;
---
+++ public/js/main.js
@@ -0,0 +1,86 @@
+var myApp = angular.module('myApp', ['ui.bootstrap']);
+
+myApp.factory('EchoScoket', function($rootScope, $log){
+
+    var service = {};
+    var ws;
+
+    service = {
+        // websocket path
+        path: "",
+
+        //
+        messageList: [],
+
+        // 接続
+        connect: function(){
+            // オブジェクト生成
+            ws = new WebSocket(service.path);
+            $log.log(ws);
+
+            // 接続
+            ws.onopen = function(){
+                $log.log("websocket connect");
+            };
+
+            // メッセージ受信
+            ws.onmessage = function(message){
+                $log.log("websocket connect")
+
+                var res = JSON.parse(message.data);
+
+                // 先頭に追加する
+                service.messageList.unshift(res);
+
+                // すぐに反映されないのでここで同期する
+                $rootScope.$apply(service.messageList);
+            };
+
+            // 切断
+            ws.onclose = function(){
+                $log.log("websocket disconnect")
+            }
+
+        },
+
+        // メッセージ送信
+        send: function(message){
+            if(message != ""){
+                ws.send(message);
+            }
+        }
+    };
+
+    return service;
+});
+
+myApp.controller('MainCtrl', function($scope, $log, EchoScoket){
+
+    $scope.EchoScoket = EchoScoket;
+    $scope.message = "";
+
+    // message clear
+    $scope.messageClear = function(){
+        $scope.message = "";
+    }
+
+    // message send
+    $scope.messageSend = function(){
+        EchoScoket.send($scope.message);
+        $scope.message = "";
+    }
+
+    // enter key send
+    $scope.handleKeydown = function(e) {
+        if (e.which == 13) {
+            EchoScoket.send($scope.message);
+            $scope.message = '';
+        }
+    }
+
+    // 初回にのみ実行され、websocketの接続処理を実施
+    $scope.$watch('EchoScoket.path', function(newVal, oldVal) {
+        EchoScoket.connect();
+    });
+});
+
